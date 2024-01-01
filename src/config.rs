8db1e533bb5da1beaa75c50c94e12cfe37f74109
Josh Vander Hook <josh@vanderhook.info>
2024-01-01 08:55:27 -0600

    session types

---
+++ src/config.rs
@@ -0,0 +1,96 @@
+
+use directories::ProjectDirs;
+
+pub struct Config {
+    pub username: String,
+    pub password: String,
+    pub endpoint: String,
+}
+
+pub fn load_all() -> Config {
+
+    let mut retval:Config = Config {
+        username: String::from(""),
+        password: String::from(""),
+        endpoint: String::from(""),
+    };
+
+    let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
+    let config_file = config_dir.config_dir().join("config.ini");
+    let contents = match ini::Ini::load_from_file(&config_file)
+    {
+        Ok(contents) => contents,
+        Err(_) => {
+            ini::Ini::new()
+        }
+    };
+
+    //does the config have a username and password?
+    if let Some(user) = contents.section(Some("user".to_owned()))
+    {
+        if user.contains_key("username") && user.contains_key("password")
+        {
+            retval.username = user.get("username").unwrap().to_owned();
+            retval.password = user.get("password").unwrap().to_owned();
+        }
+    }
+
+    if let Some(api) = contents.section(Some("endpoint".to_owned()))
+    {
+        if api.contains_key("endpoint")
+        {
+            retval.endpoint = api.get("endpoint").unwrap().to_owned();
+        }
+    }
+
+    if std::env::var("FLOTILLA_ENDPOINT").is_ok()
+    {
+        retval.endpoint = std::env::var("FLOTILLA_ENDPOINT").unwrap();
+    }
+    else if let Some(endpoint) = contents.section(Some("endpoint".to_owned()))
+    {
+        if endpoint.contains_key("endpoint")
+        {
+            retval.endpoint = endpoint.get("endpoint").unwrap().to_owned();
+        }
+    } else {
+        retval.endpoint = String::from("https://api.jodavaho.io/hfoptpreview/v2");
+    }
+
+
+    if std::env::var("FLOTILLA_USERNAME").is_ok()
+    {
+        retval.username = std::env::var("FLOTILLA_USERNAME").unwrap();
+    }
+
+    if std::env::var("FLOTILLA_PASSWORD").is_ok()
+    {
+        retval.password = std::env::var("FLOTILLA_PASSWORD").unwrap();
+    }
+    retval
+}
+
+pub fn save_file(config: Config) {
+    let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
+    let config_file = config_dir.config_dir().join("config.ini");
+    let mut contents = match ini::Ini::load_from_file(&config_file)
+    {
+        Ok(contents) => contents,
+        Err(_) => {
+            ini::Ini::new()
+        }
+    };
+
+    contents.with_section(Some("user".to_owned()))
+        .set("username", config.username)
+        .set("password", config.password);
+
+    contents.with_section(Some("endpoint".to_owned()))
+        .set("endpoint", config.endpoint);
+
+    //make sure the config directory exists
+    std::fs::create_dir_all(config_dir.config_dir()).expect("Application Error: Could not create configuration directory. Please file a bug!");
+    println!("Writing to {:?}", config_file);
+
+    contents.write_to_file(&config_file).expect("Failed to write config file");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,9 @@
 use clap::{Arg, Command};
-use directories::ProjectDirs;
-
-struct Config {
-    username: String,
-    password: String,
-}
-
-fn load_config() -> Config {
-
-    let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
-    let config_file = config_dir.config_dir().join("config.ini");
-    let mut contents = match ini::Ini::load_from_file(&config_file)
-    {
-        Ok(contents) => contents,
-        Err(_) => {
-            ini::Ini::new()
-        }
-    };
-
-    //does the config have a username and password?
-    if let Some(user) = contents.section(Some("user".to_owned()))
-    {
-        if user.contains_key("username") && user.contains_key("password")
-        {
-            return Config {
-                username: user.get("username").unwrap().to_owned(),
-                password: user.get("password").unwrap().to_owned(),
-            }
-        }
-    }
-
-    let username = match std::env::var("FLOTILLA_USERNAME")
-    {
-        Ok(username) => username,
-        Err(_) => {
-            //prompt for username
-            //save to ~/.flotilla
-            //return username
-            let mut username = String::new();
-            println!("Please enter your username: ");
-            std::io::stdin().read_line(&mut username).expect("Failed to read line");
-            username
-        }
-    };
-    contents.with_section(Some("user".to_owned()))
-        .set("username", &username);
-    let password = match std::env::var("FLOTILLA_PASSWORD")
-    {
-        Ok(password) => password,
-        Err(_) => {
-            //prompt for password
-            //save to ~/.flotilla
-            //return password
-            let mut password = String::new();
-            println!("Please enter your password: ");
-            std::io::stdin().read_line(&mut password).expect("Failed to read line");
-            password
-        }
-    };
-    contents.with_section(Some("user".to_owned()))
-        .set("password", &password);
-
-    //make sure the config directory exists
-    std::fs::create_dir_all(config_dir.config_dir()).expect("Application Error: Could not create configuration directory. Please file a bug!");
-    println!("Writing to {:?}", config_file);
-
-    match contents.write_to_file(config_file)
-    {
-        Ok(_) => println!("Config file written to ~/.flotilla"),
-        Err(_) => eprintln!("Unable to write config file to ~/.flotilla, proceeding without saving"),
-    }
-
-    Config {
-        username,
-        password,
-    }
-}
+mod config;
+mod session;
 
 fn main() {
-    let _ = Command::new("flotilla")
+    let matches = Command::new("flotilla")
         .version("0.1.0")
         .author("Joshua Vander Hook <hello@Jodavaho.io>")
         .about("A CLI tool for managing Flotilla projects")
@@ -87,11 +12,28 @@ fn main() {
             .required(false)
             .short('v')
             .long("verbose")
-            .help("Sets the level of verbosity"),
-        )
+            .help("Sets the level of verbosity")
+            .action(clap::ArgAction::SetTrue)
+            )
+        .arg(
+            Arg::new("setup")
+            .required(false)
+            .long("setup")
+            .help("writes new configuration file")
+            .action(clap::ArgAction::SetTrue)
+            )
         .get_matches_from(std::env::args());
-    let config = load_config();
-    println!("Hello, {}", config.username);
+
+    if matches.get_flag("setup")
+    {
+        let config = config::load_all();
+        config::save_file(config);
+        return;
+    }
+
+    let config = config::load_all();
+    eprintln!("Hello, {}", config.username);
+    eprintln!("Loading flotilla from {}", config.endpoint);
 
     let client = reqwest::blocking::Client::new();
 
