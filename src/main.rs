51506e5e655f7686e9f9363b3b5ab25c10a01b61
Josh Vander Hook <josh@vanderhook.info>
2024-01-01 13:48:02 -0600

    loading and querying for sessions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Arg, Command};
+use chrono::Utc;
 mod config;
 mod session;
 
@@ -34,6 +35,7 @@ fn main() {
     let config = config::load_all();
     eprintln!("Hello, {}", config.username);
     eprintln!("Loading flotilla from {}", config.endpoint);
+    eprintln!("config: API endpoint:{}",config.endpoint);
 
     let client = reqwest::blocking::Client::new();
 
@@ -42,14 +44,48 @@ fn main() {
         "password": config.password,
     });
 
-    println!("{}",json_body.to_string());
-    let res = client
-        //.post("https://api.jodavaho.io/hfoptpreview/v2/user/quick_login")
-        .post("http://localhost:8000/hfoptpreview/v2/user/quick_login")
-        .header("Content-Type", "application/json")
-        .body(json_body.to_string())
-        .send()
-        .unwrap();
-    println!("{}",res.text().unwrap());
+    let mut session = session::load_session();
+    eprintln!("Session: {:?}",session);
+    if session.expired()
+    {
+        eprintln!("Session expired, logging in");
+        let auth_url = format!("{}/user/quick_login", config.endpoint);
+        eprintln!("Auth URL: {}",auth_url);
+        let res = match client
+            .post(auth_url)
+            .header("Content-Type", "application/json")
+            .body(json_body.to_string())
+            .send()
+            {
+                Ok(res) => res,
+                Err(_) => {
+                    eprintln!("Error: Could not connect to server");
+                    return;
+                }
+            };
+
+        if res.status().is_success()
+        {
+            eprintln!("Login successful");
+            let json: serde_json::Value = serde_json::from_str(&res.text().unwrap()).unwrap();
+            session.id_token = json["AuthenticationResult"]["IdToken"].to_string();
+            session.user_id = config.username.clone();
+            session.refresh_token = json["AuthenticationResult"]["RefreshToken"].to_string();
+            session.expiration_unix = 
+                json["AuthenticationResult"]["ExpiresIn"].as_i64().unwrap()+Utc::now().timestamp();
+            session::save_session(&session);
+        } else {
+            eprintln!("Error: Could not log in");
+            return;
+        }
+    }
+
+    eprintln!("Session: {:?}",session);
+
+
+
+
+
+
 
 }
