a53b427b872edf5c28c65497338dd170e5146c2d
Josh Vander Hook <josh@vanderhook.info>
2024-01-01 23:46:18 -0600

    login done

--- src/api.rs
+++ src/api.rs
@@ -4,6 +4,8 @@ use chrono::Utc;
 
 pub fn login(config: &config::Config) -> Result<session::Session, String>
 {
+
+    eprintln!("Logging in to {}", config.endpoint);
     let client = reqwest::blocking::Client::new();
 
     let json_body = serde_json::json!({
@@ -12,39 +14,34 @@ pub fn login(config: &config::Config) -> Result<session::Session, String>
     });
 
     let mut session = session::load_session();
-    eprintln!("Session: {:?}",session);
     if session.expired()
     {
         eprintln!("Session expired, logging in");
         let auth_url = format!("{}/user/quick_login", config.endpoint);
         eprintln!("Auth URL: {}",auth_url);
-        let res = match client
+        let res = client
             .post(auth_url)
             .header("Content-Type", "application/json")
             .body(json_body.to_string())
-            .send()
-            {
-                Ok(res) => res,
-                Err(x) => {
-                    eprintln!("Error: Could not connect to server");
-                    return Err(x.to_string());
-                }
-            };
+            .send();
 
-        if res.status().is_success()
+        eprintln!("Response: {:?}",res);
+
+        if res.is_err()
         {
-            eprintln!("Login successful");
-            let json: serde_json::Value = serde_json::from_str(&res.text().unwrap()).unwrap();
-            session.id_token = json["AuthenticationResult"]["IdToken"].to_string();
-            session.user_id = config.username.clone();
-            session.refresh_token = json["AuthenticationResult"]["RefreshToken"].to_string();
-            session.expiration_unix =
-                json["AuthenticationResult"]["ExpiresIn"].as_i64().unwrap()+Utc::now().timestamp();
-            session::save_session(&session);
-        } else {
-            eprintln!("Error: Could not log in");
-            return Err("Could not log in".to_string());
-        }
+            eprintln!("Error: Could not connect to server");
+            return Err(res.err().unwrap().to_string());
+        };
+        let res = res.unwrap();
+
+        eprintln!("Login successful");
+        let json: serde_json::Value = serde_json::from_str(&res.text().unwrap()).unwrap();
+        session.id_token = json["AuthenticationResult"]["IdToken"].to_string();
+        session.user_id = config.username.clone();
+        session.refresh_token = json["AuthenticationResult"]["RefreshToken"].to_string();
+        session.expiration_unix =
+            json["AuthenticationResult"]["ExpiresIn"].as_i64().unwrap()+Utc::now().timestamp();
+        session::save_session(&session);
     }
     Ok(session)
 }
