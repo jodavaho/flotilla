5ad85a44d15e268a447c7120bebae7064866ea58
Josh Vander Hook <josh@vanderhook.info>
2024-01-02 12:35:29 -0600

    refactor for modularity

--- src/config.rs
+++ src/config.rs
@@ -8,90 +8,116 @@ pub struct Config {
     pub endpoint: String,
 }
 
-pub fn load_all() -> Config {
 
-    let mut retval:Config = Config {
-        username: String::from(""),
-        password: String::from(""),
-        endpoint: String::from(""),
-    };
 
-    let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
-    let config_file = config_dir.config_dir().join("config.ini");
-    let contents = match ini::Ini::load_from_file(&config_file)
-    {
-        Ok(contents) => contents,
-        Err(_) => {
-            ini::Ini::new()
+impl Config {
+    #[allow(dead_code)]
+    pub fn new() -> Config {
+        Config {
+            username: String::from(""),
+            password: String::from(""),
+            endpoint: String::from("https://api.jodavaho.io/hfopt/v2"),
         }
-    };
+    }
 
-    //does the config have a username and password?
-    if let Some(user) = contents.section(Some("user".to_owned()))
-    {
-        if user.contains_key("username") && user.contains_key("password")
+    #[allow(dead_code)]
+    pub fn add_matches(&self, matches: clap::ArgMatches) -> &Config {
+        let mut config = Config::new();
+        if let Some(username) = matches.get_one::<String>("username") {
+            self.username = username.clone();
+        }
+        if let Some(password) = matches.get_one::<String>("password") {
+            self.password = password.clone();
+        }
+        if let Some(endpoint) = matches.get_one::<String>("endpoint") {
+            self.endpoint = endpoint.clone();
+        }
+        self
+    }
+
+    pub fn load_all(&self) -> &Config {
+
+        let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
+        let config_file = config_dir.config_dir().join("config.ini");
+        let contents = match ini::Ini::load_from_file(&config_file)
         {
-            retval.username = user.get("username").unwrap().to_owned();
-            retval.password = user.get("password").unwrap().to_owned();
-        }
-    }
+            Ok(contents) => contents,
+            Err(_) => {
+                ini::Ini::new()
+            }
+        };
 
-    if let Some(api) = contents.section(Some("api".to_owned()))
-    {
-        if api.contains_key("endpoint")
+        //does the config have a username and password?
+        if let Some(user) = contents.section(Some("user".to_owned()))
         {
-            retval.endpoint = api.get("endpoint").unwrap().to_owned();
+            if user.contains_key("username") && user.contains_key("password")
+            {
+                self.username = user.get("username").unwrap().to_owned();
+                self.password = user.get("password").unwrap().to_owned();
+            }
         }
-    }
 
-    if std::env::var("FLOTILLA_ENDPOINT").is_ok()
-    {
-        retval.endpoint = std::env::var("FLOTILLA_ENDPOINT").unwrap();
-    }
-    else if let Some(endpoint) = contents.section(Some("api".to_owned()))
-    {
-        if endpoint.contains_key("endpoint")
+        if let Some(api) = contents.section(Some("api".to_owned()))
         {
-            retval.endpoint = endpoint.get("endpoint").unwrap().to_owned();
+            if api.contains_key("endpoint")
+            {
+                self.endpoint = api.get("endpoint").unwrap().to_owned();
+            }
         }
-    } else {
-        retval.endpoint = String::from("https://api.jodavaho.io/hfoptpreview/v2");
-    }
 
-
-    if std::env::var("FLOTILLA_USERNAME").is_ok()
-    {
-        retval.username = std::env::var("FLOTILLA_USERNAME").unwrap();
-    }
-
-    if std::env::var("FLOTILLA_PASSWORD").is_ok()
-    {
-        retval.password = std::env::var("FLOTILLA_PASSWORD").unwrap();
-    }
-    retval
-}
-
-pub fn save_file(config: Config) {
-    let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
-    let config_file = config_dir.config_dir().join("config.ini");
-    let mut contents = match ini::Ini::load_from_file(&config_file)
-    {
-        Ok(contents) => contents,
-        Err(_) => {
-            ini::Ini::new()
+        if std::env::var("FLOTILLA_ENDPOINT").is_ok()
+        {
+            self.endpoint = std::env::var("FLOTILLA_ENDPOINT").unwrap();
         }
-    };
+        else if let Some(endpoint) = contents.section(Some("api".to_owned()))
+        {
+            if endpoint.contains_key("endpoint")
+            {
+                self.endpoint = endpoint.get("endpoint").unwrap().to_owned();
+            }
+        } else {
+            self.endpoint = String::from("https://api.jodavaho.io/hfoptpreview/v2");
+        }
 
-    contents.with_section(Some("user".to_owned()))
-        .set("username", config.username)
-        .set("password", config.password);
 
-    contents.with_section(Some("endpoint".to_owned()))
-        .set("endpoint", config.endpoint);
+        if std::env::var("FLOTILLA_USERNAME").is_ok()
+        {
+            self.username = std::env::var("FLOTILLA_USERNAME").unwrap();
+        }
 
-    //make sure the config directory exists
-    std::fs::create_dir_all(config_dir.config_dir()).expect("Application Error: Could not create configuration directory. Please file a bug!");
-    println!("Writing to {:?}", config_file);
+        if std::env::var("FLOTILLA_PASSWORD").is_ok()
+        {
+            self.password = std::env::var("FLOTILLA_PASSWORD").unwrap();
+        }
+        self
+    }
 
-    contents.write_to_file(&config_file).expect("Failed to write config file");
+    pub fn save_to_default(&self, ) -> Result<&Config, std::io::Error>{
+        let config_dir = ProjectDirs::from("io", "Jodavaho", "Flotilla").expect("Application Error: Could not load configuration directory. Please file a bug!");
+        let config_file = config_dir.config_dir().join("config.ini");
+        let mut contents = match ini::Ini::load_from_file(&config_file)
+        {
+            Ok(contents) => contents,
+            Err(_) => {
+                ini::Ini::new()
+            }
+        };
+
+        contents.with_section(Some("user".to_owned()))
+            .set("username", self.username)
+            .set("password", self.password);
+
+        contents.with_section(Some("endpoint".to_owned()))
+            .set("endpoint", self.endpoint);
+
+        //make sure the config directory exists
+        std::fs::create_dir_all(config_dir.config_dir()).expect("Application Error: Could not create configuration directory. Please file a bug!");
+        println!("Writing to {:?}", config_file);
+
+        match contents.write_to_file(&config_file)
+        {
+            Ok(_) => Ok(self),
+            Err(e) => Err(e)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
 
 mod interface;
 mod config;
+use config::Config;
 mod session;
 mod api;
+
+fn verb_ships(matches: clap::ArgMatches)
+{
+    let config = Config::new().load_all().add_matches(matches.clone());
+    eprintln!("Hello, {}", config.username);
+    eprintln!("Loading flotilla from {}", config.endpoint);
+    eprintln!("config: API endpoint:{}",config.endpoint);
+    let session = api::login(&config).expect("Could not log in");
+    let app = api::Flotilla { config:*config, session:session };
+    let user_data = app.get_user_data().expect("Could not get user data");
+    eprintln!("User data: {:?}", user_data);
+}
+
+fn verb_login(matches: clap::ArgMatches)
+{
+    let config = Config::new().load_all().add_matches(matches.clone());
+    api::login(&config).expect("Could not log in").save_to_default();
+    eprintln!("Hello, {}, login session saved.", config.username);
+}
+
+fn verb_setup(matches: clap::ArgMatches) {
+    let config = Config::new().add_matches(matches.clone()).save_to_default();
+}
 
 fn main() {
     let matches = interface::parse_cli();
 
-    if matches.get_flag("setup")
+    //check user verb:
+    let verb = matches.subcommand();
+    if verb.is_none()
     {
-        let config = config::load_all();
-        config::save_file(config);
+        eprintln!("No verb specified. Please see --help for more information.");
         return;
     }
 
+    match verb.unwrap().0
+    {
+        "setup" => {
+            verb_setup(matches)
+        },
+        "login" => {
+            verb_login(matches)
+        },
+        "ships" => {
+            verb_ships(matches)
+        },
+        _ => {
+            eprintln!("Unknown verb. Please see --help for more information.");
+            return;
+        }
+    }
 
-    let config = config::load_all();
-    eprintln!("Hello, {}", config.username);
-    eprintln!("Loading flotilla from {}", config.endpoint);
-    eprintln!("config: API endpoint:{}",config.endpoint);
-
-    let session = api::login(&config).expect("Could not log in");
-
-    let app = api::Flotilla { config, session };
-    let user_data = app.get_user_data().expect("Could not get user data");
-    eprintln!("User data: {:?}", user_data);
 }
